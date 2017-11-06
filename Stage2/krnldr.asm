72644bd639d699e5afe572bfb7b5ed15832087f1
Cris Pintea <cristian@pintea.net>
2017-11-06 21:33:56 +0100

    work on stage 2 and asmlibs, kernel loading doesn't yet work

--- Stage2/krnldr.asm
+++ Stage2/krnldr.asm
@@ -1,44 +1,101 @@
 bits 16
 
-org 0x000
+org 0x500
+jmp main
+
+%include "./lib/stdio.inc"
+%include "./lib/gdt.inc"
+%include "./lib/A20.inc"
+%include "./lib/fat12.inc"
+
     ; Adjust segment registers
+    main:    
+        ; prepare segments and stack
+        cli
+        xor ax, ax
+        mov ds, ax
+        mov es, ax
+        mov ax, 0x9000                              ; stack begins at 0x9000
+        mov ss, ax
+        mov sp, 0FFFFh
+        sti
 
-    cli                                         ; disable interrupts
-    mov ax, 0x0050                              ; setup registers to point to out segment
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
+        ; load GDT
+        call installGDT
 
-    ; Create stack
+        ; Enable A20
+        call EnableA20_KKbrd_Out
+        call EnableA20_Bios
 
-    mov ax, 0x0000                              ; set the stack
-    mov ss, ax
-    mov sp, 0xFFFF
-    sti       
+         ; load the kernel
+        call LoadRoot                                           ; load the root directory
 
-    mov ah, 00h
-    mov al, 02h
-    int 10h
-
-    mov si, printstr
-    call Print
-
-    cli
-    hlt
-
-    ; Prints a string
-    ; DS=>SI: 0 terminated string
-    Print:
-        lodsb                                       ; load next byte from string at SI into AL
-        or al, al                                   ; al = 0?
-        mov bl, 3fh
-        jz PrintDone                                ; Yes, jump out
-        mov ah, 0x0e                                ; No, print next
-        int 0x10    
-        jmp Print                                   ; Repeat
-    PrintDone:
-        ret
+        mov ebx, 0
+        mov bp, KERNEL_RMODE_BASE
+        mov si, KERNEL_NAME
+        call LoadFile
+        mov DWORD [ImageSize], ecx 
+        cmp ax, 0
+        je StartKernel
+        mov si, msgFailure
+        call prints16
+        mov ah, 0
+        int 0x16                                                        ; wait for keypress
+        int 0x19   
+        cli
+        hlt
 
 
-printstr db "PiOS Stage 2 Bootloader was loaded successfully!", 0x0D, 0x0A, 0x00
+
+
+        StartKernel:
+
+        ; go into pmode
+        cli
+        mov eax, cr0
+        or eax, 1
+        mov cr0, eax
+        jmp CODE_DESC:next
+        
+        bits 32
+        next:
+
+        mov ax, DATA_DESC
+        mov ds, ax
+        mov ss, ax
+        mov es, ax
+        mov esp, 90000h
+
+        ; Loading Message
+        call ClrScr32
+        mov ebx, msgStart
+        call Puts32
+
+        ; Copy kernel to 1MB
+    CopyKernel:
+        mov eax, DWORD [ImageSize]
+        movzx ebx, WORD [bpbBytesPerSector]
+        mul ebx
+        mov ebx, 4
+        div ebx
+        cld
+        mov esi, KERNEL_RMODE_BASE
+        mov edi, KERNEL_PMODE_BASE
+        mov ecx, eax
+        rep movsd
+        jmp CODE_DESC:KERNEL_PMODE_BASE
+       
+
+
+    stop:
+        cli
+        hlt
+
+msgFound db "Kernel found", 0x0A, 0x0
+msgStart db "Loading kernel", 0x0A, 0x0
+msgFailure db "Something went wrong :(", 0x0A, 0x0
+tmp db 0x0, 0x0D, 0x0A, 0x00
+ImageSize db 0
+KERNEL_NAME db "KRNL    SYS"
+KERNEL_RMODE_BASE dd 0x3000
+KERNEL_PMODE_BASE dd 0x100000
---
+++ kernel/test.asm
@@ -0,0 +1,37 @@
+org	0x100000			; Kernel starts at 1 MB
+bits	32			; 32 bit code
+ 
+jmp	Stage3			; jump to stage 3
+ 
+%include "./lib/stdio.inc"
+ 
+msg db  0x0A, 0x0A, "Welcome to Kernel Land!!", 0x0A, 0
+ 
+Stage3:
+ 
+	;-------------------------------;
+	;   Set registers		        ;
+	;-------------------------------;
+    cli
+    hlt
+ 
+	mov		ax, 0x10		; set data segments to data selector (0x10)
+	mov		ds, ax
+	mov		ss, ax
+	mov		es, ax
+	mov		esp, 90000h		; stack begins from 90000h
+ 
+	;---------------------------------------;
+	;   Clear screen and print success	;
+	;---------------------------------------;
+ 
+	call		ClrScr32
+	mov		ebx, msg
+	call		Puts32
+ 
+	;---------------------------------------;
+	;   Stop execution			;
+	;---------------------------------------;
+ 
+	cli
+	hlt
